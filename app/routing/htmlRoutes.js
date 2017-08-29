c2e76c645a70679c41b26c03856427e967a56b77
Hannah Patellis <hannah@lawn-143-215-49-169.lawn.gatech.edu>
2017-08-29 19:46:18 -0400

    Initial UI

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,6 +5,15 @@ module.exports = function(app) {
   app.get("/play", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/play.html"));
   });
+  app.get("/upload", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/upload.html"));
+  });
+  app.get("/myaccount", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/history.html"));
+  });
+  app.get("/create", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/create.html"));
+  });
 
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public", req.url));
