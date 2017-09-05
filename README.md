82f5a38e037fb0917369a86b8a5cf2f03a8f54d2
Hannah Patellis <hannah@xanadu.lan>
2017-09-04 20:18:06 -0400

    Added webcam uploads

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # PicturePerfectPlaylist
 
 Bootcamp Project 2 by Hannah and Cj
+
+## APIs and tools used:
+- webcamjs by @jhuckaby
+- Materialize
+
+## Technologies used
+- Node.js
+- Heroku
+- JawsDB/MySQL
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -2,24 +2,19 @@ var path = require("path");
 
 module.exports = function (app, passport) {
 
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/upload',
-        failureRedirect: '/signup'
-    }
+    app.post('/signup', passport.authenticate('local-signup',
+        {
+            successRedirect: '/upload',
+            failureRedirect: '/signup'
+        }
     ));
-    app.post('/signin', passport.authenticate('local-signin', {
-        successRedirect: '/upload',
-        failureRedirect: '/signin'
-    }
+    app.post('/signin', passport.authenticate('local-signin',
+        {
+            successRedirect: '/upload',
+            failureRedirect: '/signin'
+        }
     ));
-
-
-
-
+    app.post('/upload', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/play.html"));
+    });
 }
-
-
-
-
-
-
